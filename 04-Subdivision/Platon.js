02893d4aefab0f85167bbab7f73ac46399010106
Eliott Vincent <eliott.20100@gmail.com>
2017-10-27 08:45:09 +0200

    04 subdivision

---
+++ 04-Subdivision/Platon.js
@@ -0,0 +1,185 @@
+﻿// Définition de la classe Platon
+
+Requires("Mesh");
+
+
+class Platon extends Mesh
+{
+    /** constructeur */
+    constructor()
+    {
+        super("Platon");
+
+        /** maillage */
+
+
+        // constantes
+        const a = 0.525731112119133606;
+        const b = 0.850650808352039932;
+        const c = 0.577350269;
+        const platonic_type = 2;
+
+        // sommets
+        let A,B,C,D,E,F,G,H,I,J,K,L;
+
+        // coordonnées des sommets
+        switch (platonic_type) {
+            case 0:
+                // tétraèdre
+                A = new Vertex(this,  c, c, c);
+                B = new Vertex(this, -c,-c, c);
+                C = new Vertex(this,  c,-c,-c);
+                D = new Vertex(this, -c, c,-c);
+
+                // ajout des triangles de cette forme
+                new Triangle(this, A,B,C);
+                new Triangle(this, B,A,D);
+                new Triangle(this, B,D,C);
+                new Triangle(this, A,C,D);
+
+                break;
+
+            case 1:
+                // octaèdre
+                A = new Vertex(this,  0, 0,-1);
+                B = new Vertex(this,  1, 0, 0);
+                C = new Vertex(this,  0,-1, 0);
+                D = new Vertex(this, -1, 0, 0);
+                E = new Vertex(this,  0, 1, 0);
+                F = new Vertex(this,  0, 0, 1);
+
+                // ajout des triangles de cette forme
+                new Triangle(this, A,B,C);
+                new Triangle(this, A,C,D);
+                new Triangle(this, A,D,E);
+                new Triangle(this, A,E,B);
+                new Triangle(this, F,C,B);
+                new Triangle(this, F,D,C);
+                new Triangle(this, F,E,D);
+                new Triangle(this, F,B,E);
+
+                break;
+
+            case 2:
+                // icosaèdre
+                A = new Vertex(this,  a, 0,-b);
+                B = new Vertex(this, -a, 0,-b);
+                C = new Vertex(this,  a, 0, b);
+                D = new Vertex(this, -a, 0, b);
+                E = new Vertex(this,  0,-b,-a);
+                F = new Vertex(this,  0,-b, a);
+                G = new Vertex(this,  0, b,-a);
+                H = new Vertex(this,  0, b, a);
+                I = new Vertex(this, -b,-a, 0);
+                J = new Vertex(this,  b,-a, 0);
+                K = new Vertex(this, -b, a, 0);
+                L = new Vertex(this,  b, a, 0);
+
+                // ajout des triangles de cette forme
+                new Triangle(this, A,E,B);
+                new Triangle(this, A,J,E);
+                new Triangle(this, J,F,E);
+                new Triangle(this, E,F,I);
+                new Triangle(this, E,I,B);
+                new Triangle(this, I,K,B);
+                new Triangle(this, I,D,K);
+                new Triangle(this, F,D,I);
+                new Triangle(this, F,C,D);
+                new Triangle(this, C,H,D);
+                new Triangle(this, H,K,D);
+                new Triangle(this, H,G,K);
+                new Triangle(this, H,L,G);
+                new Triangle(this, L,A,G);
+                new Triangle(this, A,B,G);
+                new Triangle(this, G,B,K);
+                new Triangle(this, J,A,L);
+                new Triangle(this, J,L,C);
+                new Triangle(this, J,C,F);
+                new Triangle(this, H,C,L);
+
+                break;
+        }
+
+        // subdiviser tous les triangles (décommenter les 1 à 4 lignes quand ça marchera)
+        //this.subdivide(true);
+        //this.subdivide(true);
+        //this.subdivide(true);
+        //this.subdivide(true);
+
+        // calculer les normales (elles ne sont pas bonnes si les points milieu sont distincts)
+        this.computeNormals();
+
+        // VBOs
+        this.buildVBOs();
+        this.buildEdgesVBO();
+
+        /** shader */
+
+        let srcVertexShader = dedent
+            `#version 300 es
+            const vec3 L = vec3(0.5, 1.0, 1.0);
+            uniform mat4 matPVM;
+            uniform mat3 matN;
+            in vec3 glVertex;
+            in vec3 glNormal;
+            out vec3 frgN;
+            out vec3 frgL;
+            void main()
+            {
+                gl_Position = matPVM * vec4(glVertex, 1.0);
+                frgN = matN * glNormal;
+                frgL = matN * L;
+            }`;
+
+        let srcFragmentShader = dedent
+            `#version 300 es
+            precision mediump float;
+            in vec3 frgN;
+            in vec3 frgL;
+            uniform vec3 color;
+            out vec4 glFragColor;
+            void main()
+            {
+                vec3 N = normalize(frgN);
+                vec3 L = normalize(frgL);
+                float dotNL = clamp(dot(N, L), 0.0, 1.0);
+                glFragColor = vec4(color, 1.0) * dotNL;
+            }`;
+
+
+        // compiler le shader de dessin
+        this.m_ShaderId = Utils.makeShaderProgram(srcVertexShader, srcFragmentShader, this.m_Name);
+
+        // déterminer où sont les variables attribute et uniform
+        this.m_MatPVMLoc      = gl.getUniformLocation(this.m_ShaderId, "matPVM");
+        this.m_MatNLoc        = gl.getUniformLocation(this.m_ShaderId, "matN");
+        this.m_ConstColorLoc  = gl.getUniformLocation(this.m_ShaderId, "color");
+        this.m_VertexLoc      = gl.getAttribLocation(this.m_ShaderId, "glVertex");
+        this.m_NormalLoc      = gl.getAttribLocation(this.m_ShaderId, "glNormal");
+        this.m_ColorLoc       = null; // VBO des couleurs pas utilisé
+    }
+
+
+    /**
+     * change la couleur de dessin
+     * @param color : fournir un vec3(r,g,b)
+     */
+    setColor(color)
+    {
+        gl.useProgram(this.m_ShaderId);
+        vec3.glUniform(this.m_ConstColorLoc, color);
+        gl.useProgram(null);
+    }
+
+
+    /**
+     * supprime toutes les ressources allouées dans le constructeur
+     */
+    destroy()
+    {
+        super.destroy();
+
+        // supprimer le shader
+        Utils.deleteShaderProgram(this.m_ShaderId);
+    }
+}
---
+++ 04-Subdivision/Scene.js
@@ -0,0 +1,76 @@
+﻿// Définition de la classe Scene
+
+// superclasses et classes nécessaires
+Requires("Platon");
+
+var colorRed   = vec3.fromValues(1.0, 0.0, 0.2);
+var colorBlack = vec3.fromValues(0.0, 0.0, 0.0);
+
+
+class Scene
+{
+    /** constructeur */
+    constructor()
+    {
+        // créer les objets à dessiner
+        this.m_Platon = new Platon();
+
+        // couleur du fond : gris très clair
+        gl.clearColor(0.9, 0.9, 0.9, 1.0);
+
+        // activer le depth buffer
+        gl.enable(gl.DEPTH_TEST);
+
+        // activer la suppression des faces cachées
+        gl.enable(gl.CULL_FACE);
+        gl.cullFace(gl.BACK);
+
+        // matrices
+        this.m_MatP = mat4.create();
+        this.m_MatV = mat4.create();
+        this.m_MatVM = mat4.create();
+    }
+
+
+    onSurfaceChanged(width, height)
+    {
+        // met en place le viewport
+        gl.viewport(0, 0, width, height);
+
+        // matrice de projection
+        mat4.perspective(this.m_MatP, Utils.radians(18.0), width / height, 0.1, 30.0);
+    }
+
+
+    onDrawFrame()
+    {
+        // effacer l'écran
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
+        // positionner la caméra
+        mat4.identity(this.m_MatV);
+        mat4.translate(this.m_MatV, this.m_MatV, vec3.fromValues(0, 0.0, -8));
+
+        // rotation demandée par la souris
+        mat4.rotateX(this.m_MatV, this.m_MatV, Utils.radians(0.0));
+        mat4.rotateY(this.m_MatV, this.m_MatV, Utils.radians(0.0));
+
+        // dessiner les triangles de la plaque légèrement en arrière
+        gl.enable(gl.POLYGON_OFFSET_FILL);
+        gl.polygonOffset(1.0, 1.0);
+        this.m_Platon.setColor(colorRed);
+        this.m_Platon.onDraw(this.m_MatP, this.m_MatV);
+        gl.disable(gl.POLYGON_OFFSET_FILL);
+
+        // dessiner les arêtes des triangles en noir
+        this.m_Platon.setColor(colorBlack);
+        this.m_Platon.onDrawEdges(this.m_MatP, this.m_MatV);
+    }
+
+
+    /** supprime tous les objets de cette scène */
+    destroy()
+    {
+        this.m_Platon.destroy();
+    }
+}

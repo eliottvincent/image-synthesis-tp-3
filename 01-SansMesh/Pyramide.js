372b87c088aaaa1c7ee0053d7143a4792883bf1f
Eliott Vincent <eliott.20100@gmail.com>
2017-10-27 08:44:36 +0200

    01 sans mesh

---
+++ 01-SansMesh/Pyramide.js
@@ -0,0 +1,144 @@
+﻿// Définition de la classe Pyramide
+
+
+class Pyramide
+{
+    /** constructeur */
+    constructor()
+    {
+        /** shader */
+
+        // version WebGL2
+
+        let srcVertexShader = dedent
+            `#version 300 es
+            uniform mat4 matrix;
+            in vec3 glVertex;
+            in vec3 glColor;
+            out vec3 frgColor;
+            void main()
+            {
+                gl_Position = matrix * vec4(glVertex, 1.0);
+                frgColor = glColor;
+            }`;
+
+        let srcFragmentShader = dedent
+            `#version 300 es
+            precision mediump float;
+            const float FogDistance = 12.0;
+            const vec3 fog_color = vec3(0.9, 0.9, 0.9);
+            in vec3 frgColor;
+            out vec4 glFragColor;
+            void main()
+            {
+                // distance entre l'écran et le fragment
+                float dist = gl_FragCoord.z / gl_FragCoord.w;
+                //glFragColor = vec4(dist - 9.0, 9.0 - dist, 0, 1);return;
+                // taux de brouillard en fonction de la distance
+                float fog = clamp((dist-FogDistance)/FogDistance, 0.0, 1.0);
+                //glFragColor = vec4(fog, fog, fog, 1);return;
+                // mélange couleur et brouillard
+                glFragColor = vec4(mix(frgColor, fog_color, fog), 1.0);
+            }`;
+
+        // compiler le shader de dessin
+        this.m_ShaderId = Utils.makeShaderProgram(srcVertexShader, srcFragmentShader, "Triangle");
+
+        // déterminer où sont les variables attribute et uniform
+        this.m_MatrixLoc = gl.getUniformLocation(this.m_ShaderId, "matrix");
+        this.m_VertexLoc = gl.getAttribLocation(this.m_ShaderId, "glVertex");
+        this.m_ColorLoc = gl.getAttribLocation(this.m_ShaderId, "glColor");
+
+
+        /** VBOs */
+
+        // créer et remplir le buffer des coordonnées
+        let vertices = [
+             0.0, 2.0,  0.0,    // P0 vert
+             1.0, 0.0,  0.0,    // P1 orange
+             0.0, 0.0, -1.0,    // P2 bleu
+            -1.0, 0.0,  0.0,    // P3 violet
+             0.0, 0.0,  1.0     // P4 bleu foncé
+        ];
+        this.m_VertexBufferId = Utils.makeFloatVBO(vertices, gl.ARRAY_BUFFER, gl.STATIC_DRAW);
+
+        // créer et remplir le buffer des couleurs
+        let colors = [
+            0.0, 0.9, 0.0,  // P0 vert
+            1.0, 0.5, 0.0,  // P1 orange
+            0.0, 0.5, 1.0,  // P2 bleu
+            0.7, 0.0, 0.7,  // P3 violet
+            0.0, 0.0, 0.9   // P4 bleu foncé
+        ];
+        this.m_ColorBufferId = Utils.makeFloatVBO(colors, gl.ARRAY_BUFFER, gl.STATIC_DRAW);
+
+        // créer et remplir le buffer des indices
+        // attention, l'ordre est inversé car il n'y a pas de projection perspective, donc
+        // on est en repère main gauche
+        let indexlist = [
+            0, 1, 2,
+            0, 2, 3,
+            0, 3, 4,
+            0, 4, 1,
+            4, 3, 2,
+            2, 1, 4
+        ];
+        this.TRIANGLE_COUNT = indexlist.length / 3;
+        this.m_IndexBufferId = Utils.makeShortVBO(indexlist, gl.ELEMENT_ARRAY_BUFFER, gl.STATIC_DRAW);
+
+        // matrice de transformation intermédiaire
+        this.m_MatPVM = mat4.create();      // P * V * M
+    }
+
+
+    /**
+     * dessiner l'objet
+     * @param matP : matrice de projection perpective
+     * @param matV : matrice de transformation de l'objet par rapport à la caméra
+     */
+    onDraw(matP, matV)
+    {
+        // activer le shader
+        gl.useProgram(this.m_ShaderId);
+
+        // fournir la matrice P * V * M au shader
+        mat4.mul(this.m_MatPVM, matP, matV);
+        mat4.glUniformMatrix(this.m_MatrixLoc, this.m_MatPVM);
+
+        // activer et lier le buffer contenant les coordonnées
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.m_VertexBufferId);
+        gl.enableVertexAttribArray(this.m_VertexLoc);
+        gl.vertexAttribPointer(this.m_VertexLoc, Utils.VEC3, gl.FLOAT, gl.FALSE, 0, 0);
+
+        // activer et lier le buffer contenant les couleurs
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.m_ColorBufferId);
+        gl.enableVertexAttribArray(this.m_ColorLoc);
+        gl.vertexAttribPointer(this.m_ColorLoc, Utils.VEC3, gl.FLOAT, gl.FALSE, 0, 0);
+
+        // activer et lier le buffer contenant les indices
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.m_IndexBufferId);
+
+        // dessiner les triangles
+        gl.drawElements(gl.TRIANGLES, this.TRIANGLE_COUNT * 3, gl.UNSIGNED_SHORT, 0);
+
+        // désactiver les buffers
+        gl.disableVertexAttribArray(this.m_VertexLoc);
+        gl.disableVertexAttribArray(this.m_ColorLoc);
+        gl.bindBuffer(gl.ARRAY_BUFFER, null);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+        // désactiver le shader
+        gl.useProgram(null);
+    }
+
+
+    /** destructeur */
+    destroy()
+    {
+        // supprimer le shader et les VBOs
+        Utils.deleteShaderProgram(this.m_ShaderId);
+        Utils.deleteVBO(this.m_VertexBufferId);
+        Utils.deleteVBO(this.m_ColorBufferId);
+        Utils.deleteVBO(this.m_IndexBufferId);
+    }
+}
---
+++ 01-SansMesh/Scene.js
@@ -0,0 +1,84 @@
+﻿// Définition de la classe Scene
+
+// superclasses et classes nécessaires
+Requires("Grid");
+Requires("Triedre");
+Requires("Pyramide");
+
+
+class Scene
+{
+    /** constructeur */
+    constructor()
+    {
+        // créer les objets à dessiner
+        this.m_Grid = new Grid();
+        this.m_Triedre = new Triedre();
+        this.m_Pyramide = new Pyramide();
+
+        // couleur du fond : gris très clair
+        gl.clearColor(0.9, 0.9, 0.9, 1.0);
+
+        // activer le depth buffer
+        gl.enable(gl.DEPTH_TEST);
+
+        // activer la suppression des faces cachées
+        gl.enable(gl.CULL_FACE);
+        gl.cullFace(gl.BACK);
+
+        // matrices
+        this.m_MatP = mat4.create();
+        this.m_MatV = mat4.create();
+        this.m_MatVM = mat4.create();
+    }
+
+
+    /**
+     * appelée quand la taille de la vue OpenGL change
+     * @param width : largeur en nombre de pixels de la fenêtre
+     * @param height : hauteur en nombre de pixels de la fenêtre
+     */
+    onSurfaceChanged(width, height)
+    {
+        // met en place le viewport
+        gl.viewport(0, 0, width, height);
+
+        // matrice de projection
+        mat4.perspective(this.m_MatP, Utils.radians(18.0), width / height, 0.1, 30.0);
+    }
+
+
+    /**
+     * Dessine l'image courante
+     */
+    onDrawFrame()
+    {
+        // effacer l'écran
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
+        // positionner la caméra à 15 unités de distance
+        mat4.identity(this.m_MatV);
+        mat4.translate(this.m_MatV, this.m_MatV, vec3.fromValues(0, 0, -15));
+
+        // rotation pour orienter la scène
+        mat4.rotateX(this.m_MatV, this.m_MatV, Utils.radians(20.0));
+        mat4.rotateY(this.m_MatV, this.m_MatV, Utils.radians(-30.0));
+
+        // dessiner la grille et les axes
+        this.m_Grid.onDraw(this.m_MatP, this.m_MatV);
+        this.m_Triedre.onDraw(this.m_MatP, this.m_MatV, 3.0);
+
+        // dessiner la pyramide en (2, 0, 1)
+        mat4.translate(this.m_MatVM, this.m_MatV, vec3.fromValues(2, 0, 1));
+        this.m_Pyramide.onDraw(this.m_MatP, this.m_MatVM);
+    }
+
+
+    /** supprime tous les objets de cette scène */
+    destroy()
+    {
+        this.m_Pyramide.destroy();
+        this.m_Triedre.destroy();
+        this.m_Grid.destroy();
+    }
+}

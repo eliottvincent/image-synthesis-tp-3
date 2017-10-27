236354450741368397fcdb89912d81f598ba72b2
Eliott Vincent <eliott.20100@gmail.com>
2017-10-27 08:44:58 +0200

    03 extrusion

---
+++ 03-Extrusion/Plaque.js
@@ -0,0 +1,104 @@
+﻿// Définition de la classe Plaque
+
+Requires("Mesh");
+
+
+class Plaque extends Mesh
+{
+    /** constructeur */
+    constructor()
+    {
+        super("Plaque");
+
+        /** maillage */
+
+        // sommets
+        let A = new Vertex(this, -2.0, 0.0,-2.0);
+        let B = new Vertex(this,  2.0, 0.0,-2.0);
+        let C = new Vertex(this, -2.0, 0.0, 2.0);
+        let D = new Vertex(this,  2.0, 0.0, 2.0);
+        let E = new Vertex(this,  0.0, 0.0,-1.0);
+        let F = new Vertex(this, -1.0, 0.0, 0.0);
+        let G = new Vertex(this,  1.0, 0.0, 0.0);
+        let H = new Vertex(this,  0.0, 0.0, 1.0);
+
+        // triangles
+        new Triangle(this, A, E, B);
+        new Triangle(this, B, E, G);
+        new Triangle(this, B, G, D);
+        new Triangle(this, D, G, H);
+        new Triangle(this, D, H, C);
+        new Triangle(this, C, H, F);
+        new Triangle(this, C, F, A);
+        new Triangle(this, A, F, E);
+        // triangles du centre
+        let T1 = new Triangle(this, E, F, H);
+        let T2 = new Triangle(this, E, H, G);
+
+        // calculer les normales
+        this.computeNormals();
+
+        // appliquer une extrusion sur T1 et T2
+        this.extrude([T1, T2], vec3.fromValues( 0.2, 1.0, -0.1));
+        this.extrude([T1, T2], vec3.fromValues(-0.2, 1.0, 0.1));
+
+        // VBOs
+        this.buildVBOs();
+        this.buildEdgesVBO();
+
+        /** shader */
+
+        let srcVertexShader = dedent
+            `#version 300 es
+            uniform mat4 matPVM;
+            in vec3 glVertex;
+            void main()
+            {
+                gl_Position = matPVM * vec4(glVertex, 1.0);
+            }`;
+
+        let srcFragmentShader = dedent
+            `#version 300 es
+            precision mediump float;
+            uniform vec3 color;
+            out vec4 glFragColor;
+            void main()
+            {
+                // pour la mise au point de la perspective (near et far)
+                glFragColor = vec4(color, 1.0);
+            }`;
+
+
+        // compiler le shader de dessin
+        this.m_ShaderId = Utils.makeShaderProgram(srcVertexShader, srcFragmentShader, this.m_Name);
+
+        // déterminer où sont les variables attribute et uniform
+        this.m_MatPVMLoc     = gl.getUniformLocation(this.m_ShaderId, "matPVM");
+        this.m_ConstColorLoc = gl.getUniformLocation(this.m_ShaderId, "color");
+        this.m_VertexLoc     = gl.getAttribLocation(this.m_ShaderId, "glVertex");
+    }
+
+
+    /**
+     * change la couleur de dessin
+     * @param color : fournir un vec3(r,g,b)
+     */
+    setColor(color)
+    {
+        gl.useProgram(this.m_ShaderId);
+        vec3.glUniform(this.m_ConstColorLoc, color);
+        gl.useProgram(null);
+    }
+
+
+    /**
+     * supprime toutes les ressources allouées dans le constructeur
+     */
+    destroy()
+    {
+        super.destroy();
+
+        // supprimer le shader
+        Utils.deleteShaderProgram(this.m_ShaderId);
+    }
+}
---
+++ 03-Extrusion/Scene.js
@@ -0,0 +1,76 @@
+﻿// Définition de la classe Scene
+
+// superclasses et classes nécessaires
+Requires("Plaque");
+
+var colorBlue = vec3.fromValues(0,0,1);
+var colorWhite = vec3.fromValues(1,1,1);
+
+
+class Scene
+{
+    /** constructeur */
+    constructor()
+    {
+        // créer les objets à dessiner
+        this.m_Plaque = new Plaque();
+
+        // couleur du fond : gris très clair
+        gl.clearColor(0.9, 0.9, 0.9, 1.0);
+
+        // activer le depth buffer
+        gl.enable(gl.DEPTH_TEST);
+
+        // activer la suppression des faces cachées
+        gl.enable(gl.CULL_FACE);
+        gl.cullFace(gl.BACK);
+
+        // matrices
+        this.m_MatP = mat4.create();
+        this.m_MatV = mat4.create();
+        this.m_MatVM = mat4.create();
+    }
+
+
+    onSurfaceChanged(width, height)
+    {
+        // met en place le viewport
+        gl.viewport(0, 0, width, height);
+
+        // matrice de projection
+        mat4.perspective(this.m_MatP, Utils.radians(18.0), width / height, 0.1, 30.0);
+    }
+
+
+    onDrawFrame()
+    {
+        // effacer l'écran
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
+        // positionner la caméra
+        mat4.identity(this.m_MatV);
+        mat4.translate(this.m_MatV, this.m_MatV, vec3.fromValues(0, -0.5, -14));
+
+        // rotation pour orienter la scène
+        mat4.rotateX(this.m_MatV, this.m_MatV, Utils.radians(20.0));
+        mat4.rotateY(this.m_MatV, this.m_MatV, Utils.radians(-30.0));
+
+        // dessiner les triangles de la plaque légèrement en arrière
+        gl.enable(gl.POLYGON_OFFSET_FILL);
+        gl.polygonOffset(1.0, 1.0);
+        this.m_Plaque.setColor(colorBlue);
+        this.m_Plaque.onDraw(this.m_MatP, this.m_MatV);
+        gl.disable(gl.POLYGON_OFFSET_FILL);
+
+        // dessiner les arêtes des triangles en blanc
+        this.m_Plaque.setColor(colorWhite);
+        this.m_Plaque.onDrawEdges(this.m_MatP, this.m_MatV);
+    }
+
+
+    /** supprime tous les objets de cette scène */
+    destroy()
+    {
+        this.m_Plaque.destroy();
+    }
+}

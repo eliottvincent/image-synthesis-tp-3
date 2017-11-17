ec5dfd6bd2528aa2ae369e28509b9746e845f18b
Eliott Vincent <eliott.20100@gmail.com>
2017-11-17 08:25:53 +0100

    05 angles Euler : typo + click & drag to move camera

--- 05-AnglesEuler/Scene.js
+++ 05-AnglesEuler/Scene.js
@@ -25,12 +25,59 @@ class Scene
         gl.enable(gl.CULL_FACE);
         gl.cullFace(gl.BACK);
 
+        // gestion souris
+        this.m_Azimut = -30.0;
+        this.m_Elevation = 20.0;
+        this.m_Pivot = vec3.fromValues(0, 0, -15);
+        this.m_Clicked = false;
+
         // matrices
         this.m_MatP = mat4.create();
         this.m_MatV = mat4.create();
         this.m_MatVM = mat4.create();
     }
 
+    onMouseDown(btn, x, y)
+    {
+        this.m_Clicked = true;
+        this.m_MousePrecX = x;
+        this.m_MousePrecY = y;
+    }
+
+    onMouseUp(btn, x, y)
+    {
+        this.m_Clicked = false;
+    }
+
+    onMouseMove(x, y)
+    {
+        if (! this.m_Clicked) return;
+
+        this.m_Azimut += 0.3 * (x - this.m_MousePrecX);
+        this.m_Elevation += 0.3 * (y - this.m_MousePrecY);
+
+        this.m_MousePrecX = x;
+        this.m_MousePrecY = y;
+    }
+
+    onKeyDown(code)
+    {
+        switch (code) {
+            case 'Z':
+                this.m_Pivot[2] += 1;
+                break;
+            case 'S':
+                this.m_Pivot[2] -= 1;
+                break;
+            case 'Q':
+                this.m_Pivot[0] += 1;
+                break;
+            case 'D':
+                this.m_Pivot[0] -= 1;
+                break;
+
+        }
+    }
 
     onSurfaceChanged(width, height)
     {
@@ -41,7 +88,6 @@ class Scene
         mat4.perspective(this.m_MatP, Utils.radians(18.0), width / height, 0.1, 30.0);
     }
 
-
     onDrawFrame()
     {
         // effacer l'écran
@@ -49,11 +95,12 @@ class Scene
 
         // positionner la caméra
         mat4.identity(this.m_MatV);
-        mat4.translate(this.m_MatV, this.m_MatV, vec3.fromValues(0, -0.5, -15));
+        // mat4.translate(this.m_MatV, this.m_MatV, vec3.fromValues(0, -0.5, -15));
+        mat4.translate(this.m_MatV, this.m_MatV, this.m_Pivot);
 
         // rotation demandée par la souris
-        mat4.rotateX(this.m_MatV, this.m_MatV, Utils.radians(20.0));
-        mat4.rotateY(this.m_MatV, this.m_MatV, Utils.radians(-30.0));
+        mat4.rotateX(this.m_MatV, this.m_MatV, Utils.radians(this.m_Elevation));
+        mat4.rotateY(this.m_MatV, this.m_MatV, Utils.radians(this.m_Azimut));
 
         // dessiner la grille et le trièdre
         this.m_Grid.onDraw(this.m_MatP, this.m_MatV);
@@ -63,8 +110,9 @@ class Scene
         mat4.translate(this.m_MatVM, this.m_MatV, vec3.fromValues(0, 1, 0));
 
         // angles d'Euler
-        /// TODO rajouter les autres rotations et voir ce que ça donne
-        mat4.rotateY(this.m_MatVM, this.m_MatVM, Utils.radians(-15.0));
+        mat4.rotateY(this.m_MatVM, this.m_MatVM, Utils.radians(Utils.getTime() * 15));  // lacet
+        mat4.rotateX(this.m_MatVM, this.m_MatVM, Utils.radians(Utils.getTime() * 30));  // tangage
+        mat4.rotateZ(this.m_MatVM, this.m_MatVM, Utils.radians(Utils.getTime() * 45));    // roulis
 
         // dessiner un trièdre dans cette rotation
         this.m_Triedre.onDraw(this.m_MatP, this.m_MatVM, 4.0);
